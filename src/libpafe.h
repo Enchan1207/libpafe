32474a7ca6de286ba9719167ac2cbdc69d4e9539
Enchan <enchant0130@gmail.com>
2024-07-13 11:05:34 +0900

    [Modify] refactor code
    - ソースコードをフォーマット

--- src/libpafe.h
+++ src/libpafe.h
@@ -15,9 +15,9 @@ typedef unsigned short int uint16;
 /* DEFINE pasori */
 
 enum PASORI_TYPE {
-  PASORI_TYPE_S310,
-  PASORI_TYPE_S320,
-  PASORI_TYPE_S330,
+    PASORI_TYPE_S310,
+    PASORI_TYPE_S320,
+    PASORI_TYPE_S330,
 };
 
 struct tag_pasori;
@@ -25,46 +25,41 @@ struct tag_pasori;
 typedef struct tag_pasori pasori;
 /*~DEFINE pasori */
 
-typedef struct _ferica_area
-{
-  uint16 code, attr, bin;
-  struct _ferica_area *next;
+typedef struct _ferica_area {
+    uint16 code, attr, bin;
+    struct _ferica_area *next;
 } felica_area;
 
 /* DEFINE felica */
 
-struct tag_felica
-{
-  pasori *p;
-  uint16 systemcode;
-  uint8 IDm[8];
-  uint8 PMm[8];
-  uint16 area_num;
-  felica_area area[256];
-  uint16 service_num;
-  felica_area service[256];
-  struct tag_felica *next;
+struct tag_felica {
+    pasori *p;
+    uint16 systemcode;
+    uint8 IDm[8];
+    uint8 PMm[8];
+    uint16 area_num;
+    felica_area area[256];
+    uint16 service_num;
+    felica_area service[256];
+    struct tag_felica *next;
 };
 typedef struct tag_felica felica;
 
-struct tag_felica_block
-{
-  uint8 data[8];
+struct tag_felica_block {
+    uint8 data[8];
 };
 typedef struct tag_felica_block felica_block;
 
-typedef struct _felica_block_info
-{
-  uint16 service;
-  uint8 mode;
-  uint16 block;
+typedef struct _felica_block_info {
+    uint16 service;
+    uint8 mode;
+    uint16 block;
 } felica_block_info;
 
 /*~DEFINE felica */
 
-#include "pasori_command.h"
 #include "felica_command.h"
-
+#include "pasori_command.h"
 
 /* DEFINE constants */
 
@@ -74,17 +69,15 @@ typedef struct _felica_block_info
 
 #define FELICA_AREA_NUM_MAX 121
 
-#define FELICA_POLLING_ANY   0xffff
+#define FELICA_POLLING_ANY 0xffff
 #define FELICA_POLLING_SUICA 0x0003
-#define FELICA_POLLING_EDY   0xfe00
+#define FELICA_POLLING_EDY 0xfe00
 
-
-#define FELICA_SERVICE_SUICA_IN_OUT  0x108f
+#define FELICA_SERVICE_SUICA_IN_OUT 0x108f
 #define FELICA_SERVICE_SUICA_HISTORY 0x090f
 #define FELICA_SERVICE_IRUCA_HISTORY 0xde80
-#define FELICA_SERVICE_EDY_NUMBER  0x110b
+#define FELICA_SERVICE_EDY_NUMBER 0x110b
 #define FELICA_SERVICE_EDY_HISTORY 0x170f
-
 
 #define FELICA_CMD_POLLING 0
 #define FELICA_ANS_POLLING 1
@@ -113,13 +106,11 @@ typedef struct _felica_block_info
 #define FELICA_CMD_WRITE_TO_SECURE_FILE 0x16
 #define FELICA_ANS_WRITE_TO_SECURE_FILE 0x17
 
-
 #define FELICA_CMD_SEARCH_SERVICE_CODE 0x0a
 #define FELICA_ANS_SEARCH_SERVICE_CODE 0x0b
 
 #define FELICA_CMD_REQUEST_SYSTEM 0x0c
 #define FELICA_ANS_REQUEST_SYSTEM 0x0d
-
 
 #define PASORI2_CMD_SELF_DIAGNOSIS 0x52
 #define PASORI2_ANS_SELF_DIAGNOSIS 0x53
@@ -137,10 +128,9 @@ typedef struct _felica_block_info
 #define PASORI2_DIAG_CPU_FANCTION_TEST 0x03
 #define PASORI2_DIAG_POLLING_TEST_TO_CARD 0x10
 
-
 #define PASORI_ERR_PARM 1
-#define PASORI_ERR_MEM  2
-#define PASORI_ERR_COM  3
+#define PASORI_ERR_MEM 2
+#define PASORI_ERR_COM 3
 #define PASORI_ERR_DATA 4
 #define PASORI_ERR_CHKSUM 5
 #define PASORI_ERR_FORMAT 6
